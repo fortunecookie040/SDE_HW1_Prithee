10618d238b66166c0dc5d66099151d5b873bfb5d
Chloe Wang <chloewang@Chloes-MacBook-Air.local>
2026-01-23 00:07:36 -0500

    attempt 2 to fix merge

--- tests/PritheeTest.java
+++ tests/PritheeTest.java
@@ -8,17 +8,16 @@ public class PritheeTest {
 
     @Test
     public void newScript() {
+        Prithee game = new Prithee();
+        game.newScript();
+    }
+
+    @Test
+    public void playGame() {
         Scanner sc = new Scanner(System.in);
         Prithee game = new Prithee();
         game.playGame(sc);
         sc.close();
 
-    }
-
-    @Test
-    public void playGame() {
-        //ngl i'm not too sure how to junit test this. i'm not familiar with junit testing or scanners.
-        //I simply tested 3 times using the code above. 
-        //I did: 3 correct, 3 incorrect, and 2 incorrect 3 correct. 
     }
 }
